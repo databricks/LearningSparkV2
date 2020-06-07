bf8bd6c4c4f63822a1ebac4289981b1087d0aac1
Brooke Wenig <brookewenig@gmail.com>
2020-06-06 20:03:07 -0700

    Added Mlflow example

---
+++ mlflow-project-example/data/README-sf-airbnb.md
@@ -0,0 +1,23 @@
+# Airbnb Data
+
+This folder contains publicly available data from Airbnb, obtained from
+http://insideairbnb.com/get-the-data.html
+
+There are subfolders within this folder for specific data sets. See the
+individual README files within those folderrs for details.
+
+## License
+
+* The data is sourced from http://insideairbnb.com/get-the-data.html under a
+  Creative Commons CC0 1.0 Universal (CC0 1.0) "Public Domain Dedication"
+  license.
+* This data is not associated with or endorsed by Airbnb or any of Airbnb's
+  competitors.
+* The data utilizes public information compiled by InsideAirbnb from the
+  Airbnb web-site including the availability calendar for 365 days in the
+  future, and the reviews for each listing. Data was verified, cleansed,
+  analyzed and aggregated by InsideAirbnb.  Databricks has not confirmed the
+  accuracy of the data in the data set.  The dataset is provided by Databricks
+  in an unmodified form, as well as in a form subjected to additional cleansing.
+* All copyright and registered trademarks remain the property of their owners.
+
---
+++ mlflow-project-example/train.py
@@ -0,0 +1,71 @@
+from pyspark.sql import SparkSession
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import VectorAssembler, StringIndexer
+from pyspark.ml.regression import RandomForestRegressor
+from pyspark.ml.evaluation import RegressionEvaluator
+import mlflow
+import mlflow.spark
+import pandas as pd
+import click
+
+spark = SparkSession.builder.appName("App").getOrCreate()
+
+@click.command()
+@click.option("--file_path", default="data/sf-airbnb-clean.parquet", type=str)
+@click.option("--num_trees", default=20, type=int)
+@click.option("--max_depth", default=5, type=int)
+def mlflow_rf(file_path, num_trees, max_depth):
+  with mlflow.start_run(run_name="random-forest") as run:
+    # Create train/test split
+    spark = SparkSession.builder.appName("App").getOrCreate()
+    airbnbDF = spark.read.parquet(file_path)
+    (trainDF, testDF) = airbnbDF.randomSplit([.8, .2], seed=42)
+
+    # Prepare the StringIndexer and VectorAssembler
+    categoricalColumns = [field for (field, dataType) in trainDF.dtypes if dataType == "string"]
+    stages = [] 
+    for categoricalCol in categoricalColumns:
+        stages += [StringIndexer(inputCol=categoricalCol, outputCol=categoricalCol + "Index", handleInvalid="skip")]
+
+    indexCols = [c + "Index" for c in categoricalColumns]
+    numericCols = [field for (field, dataType) in trainDF.dtypes if ((dataType == "double") & (field != "price"))]
+    assemblerInputs = indexCols + numericCols
+    vecAssembler = VectorAssembler(inputCols=assemblerInputs, outputCol="features")
+    stages += [vecAssembler]
+    
+    # Log params: Num Trees and Max Depth
+    mlflow.log_param("num_trees", num_trees)
+    mlflow.log_param("max_depth", max_depth)
+
+    rf = RandomForestRegressor(labelCol="price",
+                               maxBins=40,
+                               maxDepth=max_depth,
+                               numTrees=num_trees,
+                               seed=42)
+    stages += [rf]
+    pipeline = Pipeline(stages=stages)
+
+    # Log model
+    pipelineModel = pipeline.fit(trainDF)
+    mlflow.spark.log_model(pipelineModel, "model")
+
+    # Log metrics: RMSE and R2
+    predDF = pipelineModel.transform(testDF)
+    regressionEvaluator = RegressionEvaluator(predictionCol="prediction",
+                                            labelCol="price")
+    rmse = regressionEvaluator.setMetricName("rmse").evaluate(predDF)
+    r2 = regressionEvaluator.setMetricName("r2").evaluate(predDF)
+    mlflow.log_metrics({"rmse": rmse, "r2": r2})
+
+    # Log artifact: Feature Importance Scores
+    rfModel = pipelineModel.stages[-1]
+    pandasDF = (pd.DataFrame(list(zip(vecAssembler.getInputCols(),
+                                    rfModel.featureImportances)),
+                          columns=["feature", "importance"])
+              .sort_values(by="importance", ascending=False))
+    # First write to local filesystem, then tell MLflow where to find that file
+    pandasDF.to_csv("/tmp/feature-importance.csv", index=False)
+    mlflow.log_artifact("/tmp/feature-importance.csv")
+
+if __name__ == "__main__":
+  mlflow_rf()
